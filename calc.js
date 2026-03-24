84f8b9875db47debbcd0d25b4426ed5f2bb9fcff
Dhanush Kotha <kpurnasarvesh@gmail.com>
2026-03-24 22:36:21 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,32 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll('button')
+
+let string="";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let btnValue = e.target.innerHTML;
+
+        if(btnValue == '=') {
+            try {
+                string = eval(string);
+                input.value = string;
+            }catch {
+                input.value="error";
+                string="";
+            }    
+        }
+        else if(btnValue == 'AC'){
+            string ="";
+            input.value = string;
+        }
+        else if(btnValue == 'DEL'){
+            string = string.toString().slice(0,-1);
+            input.value = string;
+        }
+        else{
+        string += btnValue;
+        input.value = string;
+        }
+    })
+});
